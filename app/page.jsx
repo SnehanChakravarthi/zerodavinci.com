d7fe749b94e9d03ae71ba9040bb8383b7245d743
Snehan Chakra Varthi <snehan@motsats.com>
2023-09-25 04:18:49 +0200

    ar commit

--- app/page.jsx
+++ app/page.jsx
@@ -13,39 +13,39 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
   const snap = useSnapshot(state);
-  const [documentHeight, setDocumentHeight] = useState(0);
-  const [viewportHeight, setViewportHeight] = useState(0);
-  const [numberOfPages, setNumberOfPages] = useState(0);
+  // const [documentHeight, setDocumentHeight] = useState(0);
+  // const [viewportHeight, setViewportHeight] = useState(0);
+  // const [numberOfPages, setNumberOfPages] = useState(0);
 
-  console.log('doc height', documentHeight);
-  console.log('view height', viewportHeight);
-  console.log('pages', numberOfPages);
+  // console.log('doc height', documentHeight);
+  // console.log('view height', viewportHeight);
+  // console.log('pages', numberOfPages);
 
-  useEffect(() => {
-    // Update the document and viewport height whenever the component mounts or the window resizes
-    const updateHeights = () => {
-      const docHeight = document.documentElement.scrollHeight;
-      const viewHeight = window.innerHeight;
+  // useEffect(() => {
+  //   // Update the document and viewport height whenever the component mounts or the window resizes
+  //   const updateHeights = () => {
+  //     const docHeight = document.documentElement.scrollHeight;
+  //     const viewHeight = window.innerHeight;
 
-      setDocumentHeight(docHeight);
-      setViewportHeight(viewHeight);
+  //     setDocumentHeight(docHeight);
+  //     setViewportHeight(viewHeight);
 
-      // Calculate the number of "pages"
-      const pages = docHeight / viewHeight;
-      setNumberOfPages(pages);
-    };
+  //     // Calculate the number of "pages"
+  //     const pages = docHeight / viewHeight;
+  //     setNumberOfPages(pages);
+  //   };
 
-    // Listen for changes in the document or viewport height
-    window.addEventListener('resize', updateHeights);
+  //   // Listen for changes in the document or viewport height
+  //   window.addEventListener('resize', updateHeights);
 
-    // Initial call to set the heights
-    updateHeights();
+  //   // Initial call to set the heights
+  //   updateHeights();
 
-    // Cleanup the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('resize', updateHeights);
-    };
-  }, []);
+  //   // Cleanup the event listener when the component unmounts
+  //   return () => {
+  //     window.removeEventListener('resize', updateHeights);
+  //   };
+  // }, []);
 
   return (
     <main className="w-screen h-screen">
